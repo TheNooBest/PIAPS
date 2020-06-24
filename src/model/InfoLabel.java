14aecbddbb860b97f407ab9569a2c6e1a197e3f5
RSK-Robin <aasrobinen6989@mail.ru>
2020-06-24 20:03:03 +0300

    added a panel with info

---
+++ src/model/InfoLabel.java
@@ -0,0 +1,34 @@
+package model;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.text.Font;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class InfoLabel extends Label {
+    public final static String FONT_PATH = "/model/resources/kenvector_future.ttf";
+    public final static String BACKGROUND_IMAGE = "/model/resources/red_label.png";
+
+    public InfoLabel(String text){
+        setPrefWidth(550);
+        setPrefHeight(200);
+        setText(text);
+        setWrapText(true);
+        setLabelFont();
+        setAlignment(Pos.CENTER);
+    }
+    private void setLabelFont(){
+        try {
+            setFont(Font.loadFont(new FileInputStream(new File(FONT_PATH)), 23));
+        } catch (FileNotFoundException e) {
+            setFont(Font.font("Verdana",23));
+        }
+    }
+}
